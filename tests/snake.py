7ef4bea7bb0cd76a38592bf722e1d409741e8caa
Alexandros Samartzis <77395762+rosi-nantis@users.noreply.github.com>
2022-05-23 14:40:06 +0200

    feature: add test for moving

--- tests/snake.py
+++ tests/snake.py
@@ -1,7 +1,15 @@
 
 
-def move(current_position, direction): 
+def move(current_position: tuple, direction: str) -> tuple: 
+    x ,y = current_position
+    if type(x) != int or type(y) != int:
+        raise Exception("x and y have to be integers") 
     if direction == 'right':
-        return (6, 5)
+        new_position = x + 1, y
+    elif direction == 'up':
+        new_position = x , y + 1
+    elif direction == 'down':
+        new_position = x , y - 1
     else:
-        return (4, 5)
+        new_position = x - 1 , y
+    return new_position
--- tests/test_snake.py
+++ tests/test_snake.py
@@ -21,19 +21,45 @@ also see: uncle Bob  "clean Code Lectures"
 """
 
 from snake import move
+import pytest
 
 
 # feature: the snake is moving in all 4 directions
+
+
+# TODO : also test random positions
 
 def test_move_left():
     position = (5,5) # x, y
     new_position = move(position, 'left')
     assert new_position == (4,5)     # assert command: assert is throughing an exception 
 
+def test_move_left_from_somewhere_else():
+    position = (5,0) # x, y
+    new_position = move(position, 'left')
+    assert new_position == (4,0)     # assert command: assert is throughing an exception 
+
 def test_move_right():
     position = (5,5) # x, y
     new_position = move(position, 'right')
     assert new_position == (6,5)     # assert command: assert is throughing an exception 
+
+
+def test_move_up():
+    position = (5,5) # x, y
+    new_position = move(position, 'up')
+    assert new_position == (5,6)     # assert command: assert is throughing an exception 
+
+def test_move_down():
+    position = (5,5) # x, y
+    new_position = move(position, 'down')
+    assert new_position == (5,4)     # assert command: assert is throughing an exception 
+
+def test_move_fraction():
+    """This is an example of code that is not supposed to work"""
+    position = (3.1415,5) # x, y
+    with pytest.raises(Exception):
+        move(position, 'down')
 
 # To run the pyteest fole just press 
 # pytest in terminal or pytest test_snake.py
